271648b84f08143e76ded12b2a6ed015a89eb54c
Alvaro Kú <alvaro.ku.dev@gmail.com>
2025-12-11 17:01:52 -0600

    feat: redirect

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterLink, Router } from '@angular/router';
+import { RouterLink, Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 import { LoaderService } from '../../services/loader.service';
@@ -16,10 +16,12 @@ export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
   submitted = false;
   errorMessage = '';
+  returnUrl: string = '/app/home';
 
   constructor(
     private formBuilder: FormBuilder,
     private router: Router,
+    private route: ActivatedRoute,
     private loaderService: LoaderService,
     private authService: AuthService
   ) {}
@@ -29,6 +31,9 @@ export class LoginComponent implements OnInit {
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]]
     });
+
+    // Obtener returnUrl de los query params si existe
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/app/home';
   }
 
   get f() {
@@ -49,7 +54,7 @@ export class LoginComponent implements OnInit {
       next: (response) => {
         this.loaderService.hideLoading();
         if (response.isSuccess) {
-          this.router.navigate(['/app/home']);
+          this.router.navigate([this.returnUrl]);
         } else {
           this.errorMessage = response.message || 'Error al iniciar sesión';
         }
